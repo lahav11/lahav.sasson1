106d6226c6691a9953cec6839d96d311ef08059b
lahav11 <88339424+lahav11@users.noreply.github.com>
2021-08-29 12:41:43 +0300

    update

---
+++ CV.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+
+function myFunction() {
+    document.getElementById("h1").innerHTML = "Lahav Sasson - Data analyst | 3rd year student of industrial engineering at BGU ";
+    document.getElementById("title").innerHTML = "Lahav Sasson - Data analyst | 3rd year student of industrial enggnring at BGU";
+}
